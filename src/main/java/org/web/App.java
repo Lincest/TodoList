39b50fb718eaa93b255caef41e19a34548779d1c
Hitrel <hitrel@outlook.com>
2020-06-24 18:57:07 +0800

    implement arguments parser and test passed

--- src/main/java/org/web/App.java
+++ src/main/java/org/web/App.java
@@ -1,9 +1,51 @@
 package org.web;
 
-public class App {
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import lombok.Getter;
+import lombok.Setter;
 
-    public static void main(String ... args) {
-        System.out.println("Hello World!");
+import java.util.List;
+
+@Getter
+@Setter
+public class App {
+    @Parameter(description = "[ToDOfile] to create new file or use exists file", arity = 1)
+    private List<String> files;
+
+    @Parameter(names = {"-a", "--add"}, description = "Usage: todolist [ToDOFile] (-a/--add) [item]")
+    private boolean add = false;
+
+    @Parameter(names = {"-d, --delete"}, description = "Usage: t0dolist [ToDOFile] (-d/--delete) [item]")
+    private boolean delete = false;
+
+    @Parameter(names = {"--done", "--complete", "-do"}, description = "Usage: todolist [ToDOFile] (--done/--complete/-c) [Number of item]")
+    private int done = 0;
+
+    @Parameter(names = {"--print", "-p"}, description = "Usage: todolist [ToDOFile] (--print/-p)")
+    private boolean print = false;
+
+    @Parameter(names = {"--time", "-t"}, description = "Usage: in [item] use --time xx/xx/xx for day, --time xx:xx for time.(default today) or --time xx/xx/xx - xx:xx for full date")
+    private String date;
+
+    @Parameter(names = {"--content", "-c"}, description = "Usage in [item] use --content xxxx for add content")
+    private String content;
+
+    @Parameter(names = {"-h", "--help"}, help = true)
+    private boolean help;
+
+    public static App main(String ... args) {
+        App app = new App();
+        JCommander.newBuilder()
+                .addObject(app)
+                .build()
+                .parse(args);
+
+        return app;
+    }
+
+    public void run() {
+        System.out.println();
     }
 
 }
--- src/test/java/org/web/MainTest.java
+++ src/test/java/org/web/MainTest.java
@@ -6,11 +6,35 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 public class MainTest {
 
     @Test
     @DisplayName("Hello World")
     void helloWorld() throws Exception {
         System.out.println("Hello World!");
+    }
+
+    @Test
+    @DisplayName("ArgumentTest")
+    void testArgument() {
+    var app = App.main(new String[]{
+            "work",
+            "--add",
+            "-do", "1",
+            "-t", "xx/xx/xx",
+            "-c", "xxxxxxx",
+    });
+
+    Assertions.assertEquals(app.getFiles(), new ArrayList<>(){{add("work");}});
+    Assertions.assertEquals(app.getContent(), "xxxxxxx");
+    Assertions.assertEquals(app.getDate(), "xx/xx/xx");
+    Assertions.assertEquals(app.getDone(), 1);
+    Assertions.assertEquals(app.isAdd(), true);
+    Assertions.assertEquals(app.isDelete(), false);
+    Assertions.assertEquals(app.isHelp(), false);
+    Assertions.assertEquals(app.isPrint(), false);;
+
     }
 }
